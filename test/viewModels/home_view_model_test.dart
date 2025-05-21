537e19ec58951f105671d54ffb5fe1e8dc953645
mallik-amaan <maman.bscs22seecs@seecs.edu.pk>
2025-05-22 02:33:39 +0500

    unit tests added

---
+++ test/viewModels/home_view_model_test.dart
@@ -0,0 +1,36 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:games_deal_tracking/data/repositories/deals_remote_data_repo_impl.dart';
+import 'package:games_deal_tracking/data/services/api_service.dart';
+import 'package:games_deal_tracking/view/viewModel/home_view_model.dart';
+
+void main() {
+  group('HomeViewModel', () {
+    final ApiService apiService = ApiService();
+    final mockDealsRemoteDataRepoImpl = DealsRemoteDataRepoImpl(apiService);
+    test('Initial state should be loading', () {
+      final viewModel = HomeViewModel(mockDealsRemoteDataRepoImpl);
+      expect(viewModel.isLoading, false);
+      expect(viewModel.errorMessage, '');
+    });
+
+    test('Fetch deals should update state correctly', () async {
+      final viewModel = HomeViewModel(mockDealsRemoteDataRepoImpl);
+      viewModel.fetchDeals();
+      expect(viewModel.isLoading, true);
+      expect(viewModel.errorMessage, null);
+    });
+
+    test('error message should be null', () async {
+      final viewModel = HomeViewModel(mockDealsRemoteDataRepoImpl);
+      expect(viewModel.isLoading, false);
+      expect(viewModel.errorMessage, '');
+    });
+
+    test("fetchDealsByTitle should update state correctly", () async {
+      final viewModel = HomeViewModel(mockDealsRemoteDataRepoImpl);
+      viewModel.fetchDealsByTitle("test");
+      expect(viewModel.isLoading, true);
+      expect(viewModel.errorMessage, null);
+    });
+  });
+}
